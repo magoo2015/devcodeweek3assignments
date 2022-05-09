8d4b7bfacd18453074b518dbf6af8c30c2ee305a
Sydney McGee <mcgee.sydney@gmail.com>
2022-05-09 15:02:09 -0500

    Got the capitalization but its just one word

---
+++ main.py
@@ -0,0 +1,8 @@
+from problem_solving_one import capitilize, reverse
+
+#reverse("Sydney")
+
+
+capitalized_word = capitilize("hello world")
+
+print(capitalized_word)
---
+++ problem_solving_one.py
@@ -0,0 +1,38 @@
+"""
+1.	Reverse a string
+a.	Write code that takes a string as input and returns the string reversed
+b.	i.e. “Hello” will be returned as “olleH”
+
+"""
+
+def reverse(word): # parameter will be string taken from input within function
+    reversed_word = "" #empty string to hold characters from loop
+
+    for index in range(len(word)-1, -1, -1):
+        reversed_word += word[index]
+    print(reversed_word)
+
+#reverse("California")
+
+
+"""
+2.	Capitalize letter
+a.	Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World”
+"""
+
+
+string_word = "Hello World"
+
+new_string_word = string_word.capitalize() # can use captilize() to capitalize first character, how can I with additional words?
+# python split string might be useful  https://www.w3schools.com/python/ref_string_split.asp
+split_string_word = string_word.split(" ") #A loop should help now.
+
+def capitilize(words):
+    new_capitilized_word = ""
+    new_split_word = words.split(" ")
+
+    for word in new_split_word:
+        new_capitilized_word += word.capitalize()
+    return new_capitilized_word
+
+#capitilize("hello world")
